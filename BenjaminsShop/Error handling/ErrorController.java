d620180dfbafe3f265eed3eb764a852ca00e0f5d
Benjamin Miller <156035893+benjaminmillertx@users.noreply.github.com>
2024-08-06 12:42:42 -0500

    Create ErrorController.java

---
+++ BenjaminsShop/Error handling/ErrorController.java
@@ -0,0 +1,17 @@
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500";
+            }
+        }
+        return "error";
+    }
+}
