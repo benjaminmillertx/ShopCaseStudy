23845b6bf5453942e85ce39c8b8f82a069a0010a
Benjamin Miller <156035893+benjaminmillertx@users.noreply.github.com>
2024-08-06 12:29:33 -0500

    Create SpringConfig.java

---
+++ BenjaminsShop/SpringSecurityConfiguation/SpringConfig.java
@@ -0,0 +1,28 @@
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    private UserDetailsService userDetailsService;
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/registration", "/js/**", "/css/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginPage("/login").permitAll()
+            .and()
+            .logout().permitAll();
+    }
+}
